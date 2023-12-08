31a332dac8fa0a8c58f1866127a07d76a1f99d56
Jemjem7 <benzjeramtinga777@gmail.com>
2023-12-09 00:06:38 +0800

    added instructor.java

--- src/Sup_person/Instructor.java
+++ src/Sup_person/Instructor.java
@@ -1,41 +1,41 @@
 package Sup_person;
 
 class Instructor {
-         private String name;
-         private int YearBirth;
-         private double salary;
+             private String name;
+             private int YearBirth;
+             private double salary;
 
-    public Instructor(String name, int YearBirth, double salary) {
-        this.name = name;
-        this.YearBirth = YearBirth;
-        this.salary = salary;
+       public Instructor(String name, int YearBirth, double salary) {
+             this.name = name;
+             this.YearBirth = YearBirth;
+             this.salary = salary;
     }
     
-    public String getName() {
-        return name;
+       public String getName() {
+             return name;
     }
-    public void setName(String name) {
-        this.name = name;
+       public void setName(String name) {
+             this.name = name;
     }
-    public int getBirthYear() {
-        return YearBirth;
-    }
-
-    public void setYearBirth(int YearBirth) {
-        this.YearBirth= YearBirth;
+       public int getBirthYear() {
+             return YearBirth;
     }
 
-    public double getSalary() {
-        return salary;
+       public void setYearBirth(int YearBirth) {
+             this.YearBirth= YearBirth;
     }
 
-    public void setSalary(double salary) {
-        this.salary = salary;
+       public double getSalary() {
+             return salary;
+    }
+
+       public void setSalary(double salary) {
+             this.salary = salary;
     }
     
 
-    public String toString() {
-        return "Name: " + name + "\nBirth Year: " +YearBirth;
+      public String toString() {
+            return "Name: " + name + "\nBirth Year: " +YearBirth;
     }
 }
 
